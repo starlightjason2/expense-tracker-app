76da566304e8947f285d21673baea2feec9d7544
Jason Starlight <Shailesh Jain>
2020-09-28 22:45:21 +0530

    fixed some bugs

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
+const config = require('./config');
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-const connectDb = require('./config/db');
-
-dotenv.config({ path: './config/config.env' });
-connectDb();
 
 const transactionsRoutes = require('./routes/transactionsRoutes');
 
 const app = express();
+
+// Middleware
 app.use(cors())
 app.use(express.json());
-
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// Connect to db
+console.log(`connecting to ${config.MONGO_URI}/${config.MONGO_DB_NAME}`)
+mongoose
+    .connect(`${config.MONGO_URI}/${config.MONGO_DB_NAME}`, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// API routes
 app.use('/api/v1/transactions',  transactionsRoutes);
 
-
+// Static files
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
-const PORT = process.env.port || 5000;
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(config.PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${config.PORT}`.yellow.bold));
